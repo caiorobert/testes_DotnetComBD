7d6662598d980cca8e307b0a5833a094c3ac22e6
Caio Robert Ribeiro Cruz <ccruz@viceri.com.br>
2022-03-08 22:13:22 -0300

    Aulas 01 e 02 do curso concluídas.

---
+++ Alura.ByteBank-aula01/Alura.ByteBank.Infraestrutura.Testes/ClienteRepositorioTestes.cs
@@ -0,0 +1,96 @@
+﻿using Alura.ByteBank.Dados.Repositorio;
+using Alura.ByteBank.Dominio.Entidades;
+using Alura.ByteBank.Dominio.Interfaces.Repositorios;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Alura.ByteBank.Infraestrutura.Testes
+{
+    public class ClienteRepositorioTestes
+    {
+        private readonly IClienteRepositorio _repositorio;
+
+        public ClienteRepositorioTestes()
+        {
+            var servico = new ServiceCollection();
+            servico.AddTransient<IClienteRepositorio, ClienteRepositorio>();
+            var provedor = servico.BuildServiceProvider();
+            _repositorio = provedor.GetService<IClienteRepositorio>();
+        }
+
+        [Fact]
+        public void TestaObterTodosClientes()
+        {
+            //Arrange
+            //Act
+            List<Cliente> lista = _repositorio.ObterTodos();
+
+            //Assert
+            Assert.NotNull(lista);
+            Assert.Equal(3, lista.Count);
+
+        }
+
+        [Fact]
+        public void TestaObterClientesPorId()
+        {
+            //Arrange
+            //Act
+            var cliente = _repositorio.ObterPorId(1);
+
+            //Assert
+            Assert.NotNull(cliente);
+
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(3)]
+        [InlineData(4)]
+        public void TestaObterClientesPorVariosId(int id)
+        {
+            //Arrange
+            //Act
+            var cliente = _repositorio.ObterPorId(id);
+
+            //Assert
+            Assert.NotNull(cliente);
+
+        }
+
+        [Fact]
+        public void TestaAtualizacaoInformacaoDeterminadoCliente()
+        {
+            //Arrange
+            var cliente = _repositorio.ObterPorId(2);
+            var nomeNovo = "João Pedro";
+            cliente.Nome = nomeNovo;
+
+            //Act
+            var atualizado = _repositorio.Atualizar(2, cliente);
+
+            //Assert
+            Assert.True(atualizado);
+        }
+
+        // Testes com Mock
+        [Fact]
+        public void TestaObterClientesMock()
+        {
+            //Arange
+            var bytebankRepositorioMock = new Mock<IByteBankRepositorio>();
+            var mock = bytebankRepositorioMock.Object;
+
+            //Act
+            var lista = mock.BuscarClientes();
+
+            //Assert
+            bytebankRepositorioMock.Verify(b => b.BuscarClientes());
+        }
+
+
+    }
+}
